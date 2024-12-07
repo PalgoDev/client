9fc7f6623f030c98b381ec2fa0436626074d0630
jaymalave <jaym.github@gmail.com>
2024-12-07 10:14:36 +0530

    ft: marker rendered and interactions complete

---
+++ packages/nextjs/actions/fetchTokens.ts
@@ -0,0 +1,24 @@
+"use server";
+
+interface FetchTokensRequest {
+  long: number;
+  lat: number;
+}
+
+export const fetchTokens = async (request: FetchTokensRequest) => {
+  console.log(request, "request to fetch tokens");
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/entity/?longitude=${request.long}&latitude=${request.lat}&limit=5`,
+    );
+    const res = await response.json();
+    console.log(res, "response");
+    return res;
+  } catch (error) {
+    console.error(error, "error from fetching tokens");
+    return {
+      error: "Error fetching tokens",
+      status: 500,
+    };
+  }
+};
